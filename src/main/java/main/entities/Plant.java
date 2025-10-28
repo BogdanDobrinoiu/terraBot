b126b8d10b27bf06fee350985f80cdbad834f793
Dobrinoiu Bogdan Mihai <dobrinoiubogdan@gmail.com>
2025-10-28 13:08:30 +0200

    getter, setter minor modification in plant class

--- src/main/java/main/entities/Plant.java
+++ src/main/java/main/entities/Plant.java
@@ -4,28 +4,19 @@ import lombok.Getter;
 import lombok.Setter;
 import main.entities.flowerTypes.*;
 
+@Getter
+@Setter
 public class Plant extends Entities {
-    @Getter
-    @Setter
     private double O2;
-
-    @Getter
-    @Setter
     private String age;
 
     @Getter
     private static final double young = 0.2;
     @Getter
-    private static double mature = 0.5;
+    private static final double mature = 0.5;
     @Getter
     private static final double old = -0.3;
-
-    @Getter
-    @Setter
     private double oxygenLevel;
-
-    @Getter
-    @Setter
     protected int probability;
 
     public Plant(double O2, String age, int  probability) {
